4d298acde32786f6f962e5422c41acddc9ff04b8
fransschuijt <fp.schuijt@gmail.com>
2023-01-12 16:00:49 +0100

    deel1

--- variables.js
+++ variables.js
@@ -3,42 +3,42 @@
 // ==========================================
 
 const start = "Let's go!";
-
+console.log(start)
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
-
+const myName = "Frans";
+console.log(myName)
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
-
+const eyeColor = "brown";
+console.log(eyeColor)
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+let myAge = "41";
+console.log(myAge)
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+let myProfession = "In between jobs";
+console.log(myProfession)
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-
-
+const myShoeSize = "46";
+console.log(myShoeSize)
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+let partnerYesNo = "yes";
+console.log(partnerYesNo)
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
-
-
+let children = "0";
+console.log(children)
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
